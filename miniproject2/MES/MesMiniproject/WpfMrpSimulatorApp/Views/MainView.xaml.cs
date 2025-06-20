56b9adbff72a932729be644b8a753efa0f70443a
pgh157 <kh813love@naver.com>
2025-06-20 16:23:10 +0900

    커밋

--- miniproject2/MES/MesMiniproject/WpfMrpSimulatorApp/Views/MainView.xaml.cs
+++ miniproject2/MES/MesMiniproject/WpfMrpSimulatorApp/Views/MainView.xaml.cs
@@ -11,5 +11,10 @@ namespace WpfMrpSimulatorApp.Views
         {
             InitializeComponent();
         }
+
+        private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true; // x버튼을 눌러도, Alt+F4를 해도 안꺼짐
+        }
     }
 }
